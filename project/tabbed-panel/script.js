334878d6e05b1d47a4b1c5b49b53e9c4cf05e2d6
Pradip Yadav <pradipkyadav06@gmail.com>
2025-08-07 23:15:33 +0530

    tabbed-panel project

--- project/tabbed-panel/script.js
+++ project/tabbed-panel/script.js
@@ -1,16 +1,40 @@
-const tabButtons = document.querySelectorAll(".tab-button");
+// const tabButtons = document.querySelectorAll(".tab-button");
+// const tabContents = document.querySelectorAll(".tab-content");
+
+// tabButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const tab = button.getAttribute("data-tab");
+
+//     // Deactivate all
+//     tabButtons.forEach((btn) => btn.classList.remove("active"));
+//     tabContents.forEach((content) => content.classList.remove("active"));
+
+//     // Activate selected
+//     button.classList.add("active");
+//     document.getElementById(tab).classList.add("active");
+//   });
+// });
+
+const buttons = document.querySelectorAll(".tab-button");
+
 const tabContents = document.querySelectorAll(".tab-content");
 
-tabButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const tab = button.getAttribute("data-tab");
+buttons.forEach((btn, index, arr) => {
+  console.log(btn, index, arr);
 
-    // Deactivate all
-    tabButtons.forEach((btn) => btn.classList.remove("active"));
-    tabContents.forEach((content) => content.classList.remove("active"));
+  btn.addEventListener("click", () => {
+    const tab = btn.getAttribute("data-tab");
 
-    // Activate selected
-    button.classList.add("active");
+    // remove active
+
+    tabContents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    buttons.forEach((button) => {
+      button.classList.remove("active");
+    });
+
+    btn.classList.add("active");
     document.getElementById(tab).classList.add("active");
   });
 });
